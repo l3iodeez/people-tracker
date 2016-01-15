53b4bfd71a7f2e3556300a2386c25c66e8dea08c
Henry Dotson <l3iodeez@gmail.com>
2016-01-15 18:55:02 -0500

    Begin writing Tracker and Person object code.

--- src/people_tracker.js
+++ src/people_tracker.js
@@ -1,10 +1,60 @@
-(function() {
-  'use strict';
-  var PeopleTracker = window.PeopleTracker = window.PeopleTracker || {};
 
-  PeopleTracker.test = function () {
-    return "test";
-  };
+var PeopleTracker = window.PeopleTracker = window.PeopleTracker || {};
 
+PeopleTracker.Tracker = function () {
+  this.people = [];
+};
 
-}());
+PeopleTracker.Tracker.prototype.handleInput = function (lineArray) {
+  var delimiter;
+  if (lineArray[0].indexOf('|') !== -1 ) {
+    delimiter = " | ";
+  } else if (lineArray[0].indexOf(',') !== -1 ) {
+    delimiter = ", ";
+  } else {
+    delimiter = " ";
+  }
+  lineArray.forEach(function (el) {
+    this.people.push(this.parseInput(el, delimiter));
+  }.bind(this));
+};
+PeopleTracker.Tracker.prototype.addPerson = function (person) {
+  this.people.push(person);
+};
+PeopleTracker.Tracker.prototype.parseInput = function (string, delimiter) {
+  var array = string.split(delimiter);
+  var opts;
+
+  if (delimiter === " | ") {
+    opts = {
+      "lname": array[0],
+      "fname": array[1],
+      "mid": array[2],
+      "sex": array[3][0] === "M" ? "Male" : "Female",
+      "fcolor": array[4],
+      "dob": new Date(array[5])
+    };
+  } else if (delimiter === ", ") {
+    opts = {
+      "lname": array[0],
+      "fname": array[1],
+      "mid": "",
+      "sex": array[2][0] === "M" ? "Male" : "Female",
+      "fcolor": array[3],
+      "dob": new Date(array[4])
+    };
+  } else if (delimiter === " ") {
+    opts = {
+      "lname": array[0],
+      "fname": array[1],
+      "mid": array[2],
+      "sex": array[3][0] === "M" ? "Male" : "Female",
+      "fcolor": array[5],
+      "dob": new Date(array[4])
+    };
+  } else {
+    throw "Invalid delimiter.";
+  }
+
+  return new PeopleTracker.Person(opts);
+};
